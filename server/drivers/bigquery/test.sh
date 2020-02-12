ccc78c03b7f03c5c06bc2b38a8354a9198e27b30
Kevin Murphy <murphyke@email.chop.edu>
2020-02-12 14:39:32 -0500

    Clean up test.sh to achieve its seeming purpose

--- server/drivers/bigquery/test.sh
+++ server/drivers/bigquery/test.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-if [ ! -z ${var+BIGQUERY_TEST_CREDENTIALS_FILE} ]; then
+if [ -z "${BIGQUERY_TEST_CREDENTIALS_FILE}" ]; then
   echo "Must set BIGQUERY_TEST_CREDENTIALS_FILE with service account json file for testing"
   exit 1
 fi
 
-if [ ! -z ${var+BIGQUERY_TEST_GCP_PROJECT_ID} ]; then
+if [ -z "${BIGQUERY_TEST_GCP_PROJECT_ID}" ]; then
   echo "Must set BIGQUERY_TEST_GCP_PROJECT_ID with Google Cloud Project Name for testing"
   exit 2
 fi
 
-if [ ! -z ${var+BIGQUERY_TEST_DATASET_NAME} ]; then
+if [ -z "${BIGQUERY_TEST_DATASET_NAME}" ]; then
   echo "Must set BIGQUERY_TEST_DATASET_NAME with BigQuery Dataset Name"
   exit 3
 fi
 
-if [ ! -z ${var+BIGQUERY_TEST_DATASET_LOCATION} ]; then
+if [ -z "${BIGQUERY_TEST_DATASET_LOCATION}" ]; then
   echo "Must set BIGQUERY_TEST_DATASET_LOCATION with Google Cloud Project Region e.g. asia-southeast1"
   exit 4
 fi
