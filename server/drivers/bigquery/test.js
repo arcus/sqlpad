c60096ef84f56af756724ee66da4f628416662df
Kevin Murphy <murphyke@email.chop.edu>
2020-02-18 09:37:33 -0500

    Fix test and add individual test messages, fwiw

--- server/drivers/bigquery/test.js
+++ server/drivers/bigquery/test.js
@@ -30,38 +30,40 @@ describe('drivers/bigquery', function() {
 
   it('implements testConnection', function() {
     return bigquery.testConnection(connection).then(results => {
-      assert(results);
-      assert(!results.incomplete);
+      assert(results, 'testConnection returns results');
+      assert(!results.incomplete, 'testConnection results are marked complete');
     });
   });
 
   it('implements getSchema', function() {
     return bigquery.getSchema(connection).then(schemaInfo => {
       const schema = schemaInfo[connection.datasetName];
-      assert(schema);
-      assert(schema.hasOwnProperty(testTable));
+      const MSG = 'schema query returns expected results';
+
+      assert(schema, MSG);
+      assert(schema.hasOwnProperty(testTable), MSG);
       const columns = schema[testTable];
-      assert.equal(columns.length, 1);
-      assert.equal(columns[0].table_schema, connection.datasetName);
-      assert.equal(columns[0].table_name, testTable);
-      assert.equal(columns[0].column_name, 'id');
-      assert.equal(columns[0].data_type, 'INTEGER');
+      assert.equal(columns.length, 1, MSG);
+      assert.equal(columns[0].table_schema, connection.datasetName, MSG);
+      assert.equal(columns[0].table_name, testTable, MSG);
+      assert.equal(columns[0].column_name, 'id', MSG);
+      assert.equal(columns[0].data_type, 'INTEGER', MSG);
     });
   });
 
-  it('implements runQuery and runs queries under a limit', function() {
+  it('implements runQuery and reports successful queries as complete that return <= maxRows', function() {
     return bigquery
       .runQuery(
         `SELECT id FROM ${connection.datasetName}.${testTable} WHERE id = 1`,
         connection
       )
       .then(results => {
-        assert(!results.incomplete);
-        assert.equal(results.rows.length, 1);
+        assert(!results.incomplete, 'results should be marked complete');
+        assert.equal(results.rows.length, 1, 'right number of rows');
       });
   });
 
-  it('implements runQuery and runs queries capped at a limit', function() {
+  it('implements runQuery and reports successful queries as incomplete that return > maxRows', function() {
     const limitedConnection = { ...connection, maxRows: 2 };
 
     return bigquery
@@ -70,8 +72,23 @@ describe('drivers/bigquery', function() {
         limitedConnection
       )
       .then(results => {
-        assert(results.incomplete);
-        assert.equal(results.rows.length, 2);
+        assert(results.incomplete, 'results should be marked incomplete');
+        assert.equal(results.rows.length, 2, 'right number of rows');
+      });
+  });
+
+  it('implements runQuery and can run queries with no maxRows defined', function() {
+    const connectionWithoutMaxRows = { ...connection };
+    delete connectionWithoutMaxRows.maxRows;
+
+    return bigquery
+      .runQuery(
+        `SELECT * FROM ${connection.datasetName}.${testTable}`,
+        connectionWithoutMaxRows
+      )
+      .then(results => {
+        assert(!results.incomplete, 'results should be marked complete');
+        assert.equal(results.rows.length, 3, 'right number of rows');
       });
   });
 
@@ -82,7 +99,7 @@ describe('drivers/bigquery', function() {
         connection
       )
       .then(results => {
-        assert(results);
+        assert(results, 'returns results');
       });
   });
 
@@ -94,13 +111,16 @@ describe('drivers/bigquery', function() {
         connection
       )
       .then(() => {
-        assert.fail('An error should have been thrown.');
+        assert.fail('An error should have been thrown for Legacy SQL.');
       })
       .catch(e => {
         error = e;
       })
       .then(() => {
-        assert(error.toString().includes('Type not found'));
+        assert(
+          error.toString().includes('Type not found'),
+          'throws an exception'
+        );
       });
   });
 
@@ -112,13 +132,18 @@ describe('drivers/bigquery', function() {
         connection
       )
       .then(() => {
-        assert.fail('An error should have been thrown.');
+        assert.fail(
+          'An error should have been thrown when referencing a missing table.'
+        );
       })
       .catch(e => {
         error = e;
       })
       .then(() => {
-        assert(error.toString().includes('was not found'));
+        assert(
+          error.toString().includes('was not found'),
+          'throws the correct error'
+        );
       });
   });
 });
@@ -132,13 +157,16 @@ it('returns descriptive error message for bad syntax', function() {
       connection
     )
     .then(() => {
-      assert.fail('An error should have been thrown.');
+      assert.fail('An error should have been thrown for bad syntax.');
     })
     .catch(e => {
       error = e;
     })
     .then(() => {
-      assert(error);
-      assert(error.toString().includes('Syntax error'));
+      assert(error, 'An error is thrown for bad syntax');
+      assert(
+        error.toString().includes('Syntax error'),
+        'throws the correct error'
+      );
     });
 });
